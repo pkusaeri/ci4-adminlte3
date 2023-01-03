d6c066dced7cd0165eaa9a53707ebecc831288d4
pkusaeri <pkusaeri06@gmail.com>
2023-01-03 07:57:30 +0700

    Partial Pages Menu

---
+++ app/Controllers/Pages.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controllers;
+
+class Pages extends BaseController
+{
+    public function index()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function invoice()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function profile()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function ecommerce()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function projects()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function projectsadd()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function projects()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function projects()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function projects()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+    public function projects()
+    {
+        $data['title'] = 'Chartjs';
+        return view('templates/dashboard/v1/header',$data)
+        .view('templates/pages/chartjs')
+        .view('templates/dashboard/v1/footer');
+    }
+}
